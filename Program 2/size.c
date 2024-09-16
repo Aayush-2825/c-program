d8175037d9c0bc1a8e67f4edb80a50db2c2e97fd
Aayush-2825 <aayush.ardy@gmail.com>
2024-09-16 09:13:58 +0530

    Update size.c

--- Program 2/size.c
+++ Program 2/size.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
 
 int main(){
+    printf("Size of int is %d\n",sizeof(int));
+    printf("Size of float is %d\n",sizeof(float));
+    printf("Size of double is %d\n",sizeof(double));
+    printf("Size of char is %d\n",sizeof(char));
 }
